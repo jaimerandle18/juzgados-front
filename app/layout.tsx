3077ccfa61b821648b15d971875ca0970349f10b
jaime randle <jaimerandle18@gmail.com>
2025-12-26 16:01:31 -0300

    New

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
 import type { ReactNode } from "react";
 import AuthGuard from "./utils/AuthGuard";
+import { Capacitor } from "@capacitor/core";
+import { StatusBar, Style } from "@capacitor/status-bar";
+import { App } from "@capacitor/app";
 
 export const metadata = {
   title: "Data Jury",
@@ -10,6 +13,21 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  if (Capacitor.isNativePlatform()) {
+    StatusBar.setOverlaysWebView({ overlay: false });
+    StatusBar.setStyle({ style: Style.Light });
+  }
+
+
+if (Capacitor.isNativePlatform()) {
+  App.addListener("backButton", ({ canGoBack }) => {
+    if (canGoBack) {
+      window.history.back();
+    } else {
+      App.exitApp();
+    }
+  });
+}
   return (
     <html lang="es">
       <body className="bg-gray-100 min-h-screen">
