1b7d7dd31c826453e7e643462e46b116fff9cb65
jaime randle <jaimerandle18@gmail.com>
2026-02-03 00:46:43 -0300

    loader

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {
-        colors: {
-          fondo: "#121212",
-          rojo: "#B71C1C",
-          grisOscuro: "#1E1E1E",
-          grisClaro: "#B0B0B0",
-          azulMarino: '#1f5691', 
-        },
-        fontFamily: {
-          sans: ["Inter", "system-ui", "sans-serif"],
-        },
-        boxShadow: {
-          card: "0 4px 12px rgba(0, 0, 0, 0.4)",
-        },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",        // Next App Router
+    "./pages/**/*.{js,ts,jsx,tsx}",      // Next Pages Router
+    "./components/**/*.{js,ts,jsx,tsx}", // si tenés carpeta components
+    "./src/**/*.{js,ts,jsx,tsx}",        // lo que ya tenías
+  ],
+  theme: {
+    extend: {
+      colors: {
+        fondo: "#121212",
+        rojo: "#B71C1C",
+        grisOscuro: "#1E1E1E",
+        grisClaro: "#B0B0B0",
+        azulMarino: "#1f5691",
+      },
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+      },
+      boxShadow: {
+        card: "0 4px 12px rgba(0, 0, 0, 0.4)",
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
